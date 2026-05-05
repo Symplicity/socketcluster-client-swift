743eaa0672486c4a650dfa79891fd489afda77db
frets1700 <panmol1700@gmail.com>
2026-05-05 14:05:36 -0400

    update tools-version and minor lint fixes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,10 @@ let package = Package(
     products: [
         .library(
             name: "ScClient",
-            targets: ["ScClient"]),
+            targets: [
+                "ScClient"
+            ]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/daltoniam/Starscream.git", .exact("3.1.1")),
@@ -16,19 +19,21 @@ let package = Package(
         .package(url: "https://github.com/Quick/Quick.git", .exact("2.2.1")),
         .package(url: "https://github.com/Quick/Nimble.git", .exact("8.0.9")),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
-        ],
+    ],
     targets: [
         .target(
             name: "ScClient",
             dependencies: [
                 "Starscream",
                 "HandyJSON"
-                ]),
+            ]
+        ),
         .target(
             name: "Main",
             dependencies: [
                 "ScClient",
-                ]),
+            ]
+        ),
         .testTarget(
             name: "Integration",
             dependencies: [
@@ -36,11 +41,13 @@ let package = Package(
                 "Quick",
                 "Nimble",
                 "RxSwift"
-                ]),
+            ]
+        ),
         .testTarget(
             name: "Unit",
             dependencies: [
                 "ScClient"
-                ])
-        ]
+            ]
+        )
+    ]
 )
